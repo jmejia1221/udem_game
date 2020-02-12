b42c57a9c5b52aa370fa78664383a611e3235904
John Mejía Hernández <johnmejiahernandez@Johns-iMac.local>
2020-02-12 01:02:20 -0500

    Added login and logout logic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
 import React from 'react';
-import './App.css';
 import Login from './components/Login/Login';
+import Home from './Containers/Home';
+import fire from './config/fire';
 
-function App() {
-  return (
-    <div className="App">
-      <Login />
-    </div>
-  );
+// CSS
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: null
+    }
+
+    this.authListener = this.authListener.bind(this);
+  }
+
+  componentDidMount() {
+    this.authListener();
+  }
+
+  authListener() {
+    fire.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ user });
+      } else {
+        this.setState({ user: null });
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.user ? (<Home />) : (<Login />)}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,10 +5,47 @@ import fire from '../../config/fire';
 import LoginStyle from './Login.css';
 
 class Login extends React.Component {
+
+    login() {
+        const email = document.querySelector("#email").value;
+        const password = document.querySelector("#password").value;
+
+        fire.auth().signInWithEmailAndPassword(email, password)
+            .then((u) => {
+                console.log('Successfully Logged in');
+            })
+            .catch((err) => {
+                console.log('Error: ', err.toString());
+            })
+    }
+
+    logout() {
+        const email = document.querySelector("#email").value;
+        const password = document.querySelector("#password").value;
+
+        fire.auth().createUserWithEmailAndPassword(email, password)
+            .then((u) => {
+                console.log('Successfully Signed up');
+            })
+            .catch((err) => {
+                console.log('Error: ', err.toString());
+            })
+
+    }
+
     render() {
         return (
             <div>
-                Hello world
+                <div>
+                    <div>Email</div>
+                    <input id="email" placeholder="Enter Email.." type="email" />
+                </div>
+                <div>
+                    <div>Password</div>
+                    <input id="password" placeholder="Enter Password" type="password" />
+                </div>
+                <button onClick={this.login}>Login</button>
+                <button onClick={this.logout}>Sign Up</button>
             </div>
         )
     }
